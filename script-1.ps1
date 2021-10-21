6b1ec03e4496ec0ec4c60dd3e76b666728f7a6f2
Juan Carlos <jcrequena.fp@gmail.com>
2021-10-21 16:41:45 +0200

    Create script-1.ps1

---
+++ script-1.ps1
@@ -0,0 +1,14 @@
+
+Import-Module ADDSDeployment
+Install-ADDSForest `
+-CreateDnsDelegation:$false `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainMode "WinThreshold" `
+-DomainName "smr.local" `
+-DomainNetbiosName "smr" `
+-ForestMode "Win2012" `
+-InstallDns:$true `
+-LogPath "C:\Windows\NTDS" `
+-NoRebootOnCompletion:$false `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true
