c357346ea0412b315a0eb671206843038796ec80
Juan Carlos <jcrequena.fp@gmail.com>
2021-10-21 16:53:05 +0200

    Update script-3.ps1

--- script-3.ps1
+++ script-3.ps1
@@ -1,15 +1,18 @@
+$nameServer="serv-resp"
+Rename-Computer -NewName $nameServer
+
 Import-Module ServerManager
 Add-WindowsFeature AD-Domain-Services
-Import-Module ADDSDeployment #Se carga el módulo
+Import-Module ADDSDeployment 
 
 Install-ADDSDomainController ` 
--DomainName "smr.local" `
+-DomainName "logo-span.com" `
 –Credential (Get-Credential) `
 –SiteName “Default-First-Site-Name” `
 –InstallDNS:$True `
 –NoGlobalCatalog:$false `
 -CreateDNSDelegation:$false `
--ReplicationSourceDC "serv-1.smr.local" `
+-ReplicationSourceDC "serv-raiz.logo-span.com" `
 –CriticalReplicationOnly:$False –DatabasePath “C:\Windows\NTDS” `
 –LogPath “C:\Windows\NTDS” `
 –SysVolPath “C:\Windows\SysVol” `
