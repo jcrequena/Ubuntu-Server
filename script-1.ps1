a57416a8dd6f7d3c3ef4b4f78178d6a0659aa02b
Juan Carlos <jcrequena.fp@gmail.com>
2021-10-21 16:47:36 +0200

    Update script-1.ps1

--- script-1.ps1
+++ script-1.ps1
@@ -3,12 +3,47 @@ Import-Module ADDSDeployment
 Install-ADDSForest `
 -CreateDnsDelegation:$false `
 -DatabasePath "C:\Windows\NTDS" `
--DomainMode "WinThreshold" `
--DomainName "smr.local" `
+-DomainMode "5" `
+-DomainName "logo-span.com" `
 -DomainNetbiosName "smr" `
--ForestMode "Win2012" `
+-ForestMode "5" `
 -InstallDns:$true `
 -LogPath "C:\Windows\NTDS" `
 -NoRebootOnCompletion:$false `
 -SysvolPath "C:\Windows\SYSVOL" `
 -Force:$true
+
+Install-ADDSDomain `
+-NoGlobalCatalog:$false `
+-CreateDnsDelegation:$true `
+-Credential (Get-Credential) `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainMode "5" `
+-DomainType "ChildDomain" `
+-InstallDns:$true `
+-LogPath "C:\Windows\NTDS" `
+-NewDomainName "delegacion1" `
+-NewDomainNetbiosName "DELEGACION1" `
+-ParentDomainName "logo-span.com" `
+-NoRebootOnCompletion:$false `
+-SiteName "Default-First-Site-Name" `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true
+
+Install-ADDSDomain `
+-NoGlobalCatalog:$false `
+-CreateDnsDelegation:$true `
+-Credential (Get-Credential) `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainMode "5" `
+-DomainType "ChildDomain" `
+-InstallDns:$true `
+-LogPath "C:\Windows\NTDS" `
+-NewDomainName "delegacion2" `
+-NewDomainNetbiosName "DELEGACION2" `
+-ParentDomainName "logo-span.com" `
+-NoRebootOnCompletion:$false `
+-SiteName "Default-First-Site-Name" `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true
+
